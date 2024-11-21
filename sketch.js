eef8b7b0a519eed3913fb1f6fc9ef1d13d601b02
TricksterAkira <nearysk03@uww.edu>
2024-11-21 11:43:43 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
 function setup() {
+  colorMode(HSL);
+  frameRate(60);
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(10);
+  push();
+  setCenter(mouseX,mouseY);
+  polarSquare(0,18);
+  pop();
+  stroke(200);
+  line(mouseX-10,mouseY+15,mouseX-10,mouseY+33);
+  line(mouseX+10,mouseY+15,mouseX+10,mouseY+33);
 }
