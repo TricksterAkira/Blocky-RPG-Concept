45dbfc33637b3f72353295805c0d450e791cd438
TricksterAkira <nearysk03@uww.edu>
2024-11-21 15:01:57 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,4 +4,19 @@ function setup() {
 
 function draw() {
   background(220);
+  //middle ground 1
+  fill(31,25,37);
+  rect(235,270,750,100);
+  fill(91,34,32);
+  rect(235,270,750,30);
+  //middle ground 2
+  fill(31,36,37);
+  rect(475,186,400,200);
+  fill(91,37,32);
+  rect(475,186,400,30);
+  //foreground
+  fill(31,40,37);
+  rect(0,360,800,50);
+  fill(91,52,32);
+  rect(0,360,800,30);
 }
