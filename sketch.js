fb49602e4f4f13f960d075ff6336f011fa32c1ab
TricksterAkira <nearysk03@uww.edu>
2024-11-21 14:31:53 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
+let sky;
+
+function preload(){
+  sky = loadImage('./assets/voltron redraw.png')
+}
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  image(sky,-550,0);
 }
