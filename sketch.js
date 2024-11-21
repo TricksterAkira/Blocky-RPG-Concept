b01cf26fd68df29621b262cb1574ff2c2ed06d99
TricksterAkira <nearysk03@uww.edu>
2024-11-21 14:09:29 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,6 +6,16 @@ function setup() {
 
 function draw() {
   background(10);
+  stroke(180);
+  line(mouseX+28,mouseY+23,mouseX+34,mouseY+7);
+  noFill();
+  beginShape();
+  vertex(mouseX+34,mouseY+7);
+  vertex(mouseX+27,mouseY+3);
+  vertex(mouseX+41,mouseY-15);
+  vertex(mouseX+41,mouseY+10);
+  vertex(mouseX+34,mouseY+7);
+  endShape();
   stroke(46,93,55.1);
   strokeWeight(3);
   line(mouseX-10,mouseY+18,mouseX-10,mouseY+33);
@@ -14,6 +24,7 @@ function draw() {
   line(mouseX+18,mouseY,mouseX+30,mouseY+10);
   push();
   noStroke();
+  fill(200);
   setCenter(mouseX,mouseY);
   polarSquare(0,18);
   pop();
